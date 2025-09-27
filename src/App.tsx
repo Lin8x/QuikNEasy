e80214607917694bc9a16ad4d69a86fe96376a25
Leonardo <leonardo.reyes0040@gmail.com>
2025-09-27 13:58:51 -0400

    importing protein
    import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import enq_logo from "./assets/enq_logo.png";
+import protein from "./assets/protein.png";
 import "./App.css";
 import Navbar from "./components/Navbar";
 
