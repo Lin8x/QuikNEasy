b1ec12f58aa8c069c7b3040f381e01276da20605
MasterShakezula <90061079+MasterShakezula@users.noreply.github.com>
2025-09-27 22:50:37 -0400

    Homepage

--- src/components/HomepageGraphics.tsx
+++ src/components/HomepageGraphics.tsx
@@ -6,38 +6,43 @@ import carbs from "../assets/carbs.png";
 const HomepageGraphics = () => {
   return (
     <div className="logo">
-    <a href={salad} target="_blank">
-      <img
-        src={salad}
-        style={{ height: "500px", width: "auto" }}
-        className="logo"
-        alt="QuikNEasySalad"
-      />
-    </a>
-    <a href="protein.png" target="_blank">
-          <img
-            src={protein}
-            style={{ height: "40px" , width: "auto"  }}
-            className="logo"
-            alt="QuikNEasyProtein"
-          />
-        </a>
-        <a href="fat.png" target="_blank">
-          <img
-            src={fat}
-            style={{ height: "40px" , width: "auto"  }}
-            className="logo"
-            alt="QuikNEasyFat"
-          />
-        </a>
-        <a href="carbs.png" target="_blank">
-          <img
-            src={carbs}
-            style={{ height: "40px" , width: "auto"  }}
-            className="logo"
-            alt="QuikNEasyCarbs"
-          />
-        </a>
+      <a href={salad} target="_blank">
+        <img
+          src={salad}
+          style={{
+            height: "400px",
+            width: "fit-content",
+            display: "block",
+            margin: "0 auto",
+          }}
+          className="logo"
+          alt="QuikNEasySalad"
+        />
+      </a>
+      <a href="protein.png" target="_blank">
+        <img
+          src={protein}
+          style={{ height: "40px", width: "auto" }}
+          className="logo"
+          alt="QuikNEasyProtein"
+        />
+      </a>
+      <a href="fat.png" target="_blank">
+        <img
+          src={fat}
+          style={{ height: "40px", width: "auto" }}
+          className="logo"
+          alt="QuikNEasyFat"
+        />
+      </a>
+      <a href="carbs.png" target="_blank">
+        <img
+          src={carbs}
+          style={{ height: "40px", width: "auto" }}
+          className="logo"
+          alt="QuikNEasyCarbs"
+        />
+      </a>
     </div>
   );
 };
